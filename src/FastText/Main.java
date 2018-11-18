21f045073a01d678ec6f6601d4470b683e8f6e05
laomobk <laomobk@163.com>
2018-11-18 16:23:47 +0800

    remove sth.

--- src/FastText/Main.java
+++ src/FastText/Main.java
@@ -4,12 +4,7 @@ public class Main
 {
 	public static void main(String[] a){
 		RUN(
-<<<<<<< HEAD
 				"src\\test\\loop.ft");
-=======
-			"/storage/emulated/0/AppProjects/FastText/src/test/loop.ft");
->>>>>>> e7c0d2ef7262bd97b37fed8f584658e7920923c8
-		
 	}
 	
 	public static void RUN(String p){
